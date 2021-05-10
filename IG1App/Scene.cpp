ba5e810f5c3b0d66de9b1f50b0ca1f8f14bba824
Nope <leonorcu@ucm.es>
2021-05-10 18:08:28 +0200

    cambiado de TrianguloRGB a Poligono

--- IG1App/Scene.cpp
+++ IG1App/Scene.cpp
@@ -93,7 +93,7 @@ void Scene::init()
 
 
 	Abs_Entity* aux;
-	aux = new TrianguloRGB(100.0);
+	aux = new Poligono(3, 100.0);
 	aux->setShader(new Shader("..\\Shaders\\3.3.shader.vs", "..\\Shaders\\3.3.shader.fs"));
 	gObjects.push_back(aux);
     // Graphics objects (entities) of the scene
